f1d8a9eb7b624843e4a9ebb2672850ca455a025f
niko <674233509@qq.com>
2018-07-05 20:25:13 +0800

    初始lar环境

---
+++ Niko/public/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>goodbye Summer</title>
+<meta name="Keywords" content="" >
+<meta name="Description" content="" >
+<link href="css/index.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="js/modernizr.js"></script>
+<![endif]-->
+</head>
+<body>
+<header>
+  <h1><a href="/">Goodbye Summer</a></h1>
+  <p>asdasd阿萨德</p>
+</header>
+<div id="nav">
+     <ul>
+       <li><a href="index.html">网站首页</a></li>
+       <li><a href="about.html">关于我</a></li>
+       <li><a href="moodlist.html">碎言碎语</a></li>
+       <li><a href="newlist.html">技术探讨</a></li>
+       <li><a href="newlist.html">慢生活</a></li>
+       <li><a href="ablumlist.html">我的相册</a></li>
+       <li><a href="book.html">留言版</a></li>
+     </ul>
+     <script src="js/silder.js"></script><!--获取当前页导航 高亮显示标题-->
+</div>
+
+</body>
+</html>
